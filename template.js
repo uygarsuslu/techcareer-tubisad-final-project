cbd01de23c4ae4bac4959e20478159bf61c7c5ac
Uygar Süslü <uygarsuslu1@gmail.com>
2022-07-29 17:48:25 +0300

    autocomplete and validation added

---
+++ template.js
@@ -0,0 +1,121 @@
+/* autocomplete */
+$(function () {
+    var availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+    ];
+    $("#tags").autocomplete({
+        source: availableTags
+    });
+});
+
+/* login value and validation*/
+$(function () {
+    $("#btnSubmit").click(function () {
+        let email, password;
+        email = jQuery.trim($("#user_email").val());
+        password = jQuery.trim($("#user_password").val());
+
+        //validation  email
+        if (email == "") {
+            $("#validation_email").html("Email boş geçilemez...")
+        }
+        else if (regexEmail(email) == false) {
+            $("#validation_email").html("Uygun formatta email girilmedi, örnek: deneme@gmail.com")
+        }
+
+        //regex email
+        function regexEmail(email) {
+            let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return regex.test(email);
+        }
+
+        //validation password
+        if (password == "") {
+            $("#validation_password").html("şifre boş geçilemez...")
+        }
+        else if (regexPassword(password) == false) {
+            $("#validation_password").html("Uygun formatta  şifre girilmedi, örnek: en az 8 karakter")
+        }
+
+        //regex password
+        function regexPassword(password) {
+            let regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;;
+            return regex.test(password);
+        }
+
+    }) // onclick close
+}); // document ready close
+
+/* register value and validation*/
+$(function () {
+    $("#btnSubmit2").click(function () {
+        let registerName, registerEmail, registerPassword;
+        registerName = jQuery.trim($("#register_name").val());
+        registerEmail = jQuery.trim($("#register_email").val());
+        registerPassword = jQuery.trim($("#register_password").val());
+
+        //validation registerName
+        if (registerName == "") {
+            $("#validation_registerName").html("Kullanıcı adı boş geçilemez...")
+        }
+        else if (regexName(registerName) == false) {
+            $("#validation_registerName").html("Kullanıcı adı uygun formatta değil, örnek: türkçe karakter içermemeli")
+        }
+
+        //regex registerName
+        function regexName(registerName) {
+            let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return regex.test(registerName);
+        }
+
+        //validation registerEmail
+        if (registerEmail == "") {
+            $("#validation_registerEmail").html("Email boş geçilemez...")
+        }
+        else if (regexEmail(registerEmail) == false) {
+            $("#validation_email").html("Uygun formatta email girilmedi, örnek: deneme@gmail.com")
+        }
+
+        //regex email
+        function regexEmail(registerEmail) {
+            let regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return regex.test(registerEmail);
+        }
+
+        //validation registerPassword
+        if (registerPassword == "") {
+            $("#validation_registerPassword").html("şifre boş geçilemez...")
+        }
+        else if (regexPassword(registerPassword) == false) {
+            $("#validation_registerPassword").html("Uygun formatta  şifre girilmedi, örnek: en az 8 karakter")
+        }
+
+        //regex registerPassword
+        function regexPassword(registerPassword) {
+            let regex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;;
+            return regex.test(registerPassword);
+        }
+
+    }) // onclick close
+}); // document ready close
