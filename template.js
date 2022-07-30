3c2026da1d4ec0101fa042baee809191160e5716
Uygar Süslü <uygarsuslu1@gmail.com>
2022-07-30 21:07:30 +0300

    date function added

--- template.js
+++ template.js
@@ -119,3 +119,27 @@ $(function () {
 
     }) // onclick close
 }); // document ready close
+
+// date function
+let date = () => {
+    // document.getElementById("date1").innerHTML = new Date()
+
+    // $('selector').action()
+    // $('#date1').text(new Date())
+    // $('#date1').html(new Date())
+    $('#date1').html(new Date())
+
+    // document.getElementById("date1").style.color = "white"
+    // document.getElementsByClassName("date1")[0].style.backgroundColor = "blue"
+
+    // $("#date1").css("background-color", "blue").css("color", "white")
+
+    const cssObject = {
+        "color": "blue",
+        "font-weight": "bold",
+        "background-color": "white",
+        "box-shadow": "1px 1px 3px 1px rgb(255,255,255,.8)",
+        "margin-bottom": "2rem"
+    }
+    $("#date1").css(cssObject)
+}
